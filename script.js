6b33cd63b55d54d3eb7ed181097199ec5ca3df05
Vamsi <vk784@snu.edu.in>
2023-02-05 17:56:15 +0530

    Fix bug where product randomly becomes 0

--- script.js
+++ script.js
@@ -32,9 +32,11 @@ let num={
     num2:0
 };
 let i=1;
+let j=1;
 let operator = '';
 let check = true;
 const operators = ['+','-','x','/','=','AC','↺']
+const numbers = ['1','2','3','4','5','6','7','8','9','0']
 const numeric_operators = ['+','-','x','/']
 d=document.getElementById('disp');
 buttons = document.querySelectorAll('button');
@@ -67,10 +69,8 @@ buttons.forEach(button => {
                 num.num1=Number(string);
             }
             else{
-                
                 num.num2=Number(string);
                 num.num1 = operate(num.operator,num.num1,num.num2)
-                //num.num1 = Number((num.num1)).toFixed(3)
             }
             num.operator = e.target.innerText;
             i++;
@@ -86,8 +86,8 @@ buttons.forEach(button => {
             else{
                 num.num2=Number(string);
                 num.num1 = operate(num.operator,num.num1,num.num2)
-                //num.num1 = Number((num.num1)).toFixed(3)
             }
+            string=''
             document.getElementById('disp').textContent = Number(num.num1).toFixed(3);
             
         }
@@ -99,6 +99,54 @@ buttons.forEach(button => {
             string='';
             check = true;
             document.getElementById('disp').textContent=string;
+        }
+    })
+    button.addEventListener('keydown',(e)=>{
+        console.log(e)
+        if((numbers.indexOf(e.key)==1)||(e.key==='Backspace')||(e.key=='.')){
+            if(e.key==='Backspace'){
+                string = string.slice(0,-1);
+            }
+            else{
+                if(e.key==='.'){
+                    if(check==true){
+                        string+=e.key
+                        check=false;
+                    }
+                }
+                else{
+                    string += e.key;
+                }    
+            }
+            document.getElementById('disp').textContent = string;
+            console.log(num.num1);
+            console.log(num.num2);
+        }
+        else if(numeric_operators.indexOf(e.key)==1){
+            check=true;
+            if(i==1){
+                num.num1=Number(string);
+            }
+            else{
+                
+                num.num2=Number(string);
+                num.num1 = operate(num.operator,num.num1,num.num2)    
+            }
+            num.operator = e.shiftKey;
+            i++;
+            if(i>2){
+                document.getElementById('disp').textContent = Number(num.num1).toFixed(3);
+            }
+        }
+        else if(e.key=='=' || (e.key=='Enter')){
+            if(i==1){
+                num.num1=Number(string);
+            }
+            else{
+                num.num2=Number(string);
+                num.num1 = operate(num.operator,num.num1,num.num2)
+            }
+            document.getElementById('disp').textContent = Number(num.num1).toFixed(3);
         }
     })
 });
